fff402499c7e05a00909b528f237433c76df3aab
Yuanhao Liu <1683309112@qq.com>
2025-05-08 16:55:28 +0800

    ''

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # FACD
 
-We provide comprehensive instructions on how to run FACD in the ***<u>"scripts"</u>*** directory. 
+The implementation for the IJCAI-25 paper "A Fast-Adaptive Cognitive Diagnosis Framework for Computerized Adaptive Testing Systems". The main paper and the appendix are in the main.pdf and appendix.pdf respectively, which are contained in the folder named paper.
 
-# Requirements	
+# 💻 Requirements	
 
 ```python
 dgl==2.1.0+cu121
@@ -22,16 +22,27 @@ Please install all the dependencies listed in the `requirements.txt` file by run
 pip install -r requirements.txt
 ```
 
-# Experiments
+# 🛠️ Experiments
 
-Firstly, you need
-
-> cd scripts
-
-Then, create a ***<u>"ckpt"</u>*** directory in ***<u>"model"</u>*** directory and you can run our framework in different dataset with random selection strategy using following command:
+You can run our framework in provided dataset with BECAT selection strategy using following command:
 
 ```shell
-bash run.sh
+bash scripts/run.sh
 ```
 
-If you want to change the CAT selection strategy, just replace the parameter of `--strategy` with the name of your target strategy, such as MAAT, NCAT, BECAT.
+If you want to change the CAT selection strategy, just replace the parameter of `--strategy` with the name of your target strategy, such as random, MAAT, NCAT.
+
+# Reference
+
+Yuanhao Liu, Yiya You, Shuo Liu, Hong Qian, Ying Qian, Aimin Zhou "A Fast-Adaptive Cognitive Diagnosis Framework for Computerized Adaptive Testing Systems" In Proceedings of the 34th International Joint Conference on Artificial Intelligence, 2025.
+
+## Bibtex
+```
+@inproceedings{liu2025facd,
+ author = {Yuanhao Liu and Yiya You and Shuo Liu and Hong Qian and Ying Qian and Aimin Zhou},
+ booktitle = {Proceedings of the 34th International Joint Conference on Artificial Intelligence (IJCAI)},
+ title = {A Fast-Adaptive Cognitive Diagnosis Framework for Computerized Adaptive Testing Systems},
+ year = {2025},
+ address = {Montreal, Canada}
+}
+```
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,17 +1,5 @@
 python train.py --dataset FrcSub --cdm facd --device cuda:0 --lr 3e-3 --batch_size 128 --num_epochs 1 --decoder irt --seed 0 --test_size 0.8
-python test.py --dataset FrcSub --strategy random --cdm facd --device cuda:0 --lr 3e-3 --batch_size 128 --num_epochs 2 --decoder irt --seed 0 --test_size 0.8 --test_length 15
+python test.py --dataset FrcSub --strategy BECAT --cdm facd --device cuda:0 --lr 3e-3 --batch_size 128 --num_epochs 2 --decoder irt --seed 0 --test_size 0.8 --test_length 15
 
 python train.py --dataset FrcSub --cdm facd --device cuda:0 --lr 3e-3 --batch_size 128 --num_epochs 1 --decoder ncd --seed 0 --test_size 0.8
-python test.py --dataset FrcSub --strategy random --cdm facd --device cuda:0 --lr 3e-3 --batch_size 128 --num_epochs 2 --decoder ncd --seed 0 --test_size 0.8 --test_length 15
-
-python train.py --dataset EDMCUP --cdm facd --device cuda:0 --lr 5e-3 --batch_size 32 --num_epochs 1 --decoder irt --seed 1 --test_size 0.8
-python test.py --dataset EDMCUP --strategy random --cdm facd --device cuda:0 --lr 5e-3 --batch_size 32 --num_epochs 2 --decoder irt --seed 1 --test_size 0.8 --test_length 15 
-
-python train.py --dataset EDMCUP --cdm facd --device cuda:0 --lr 5e-3 --batch_size 32 --num_epochs 1 --decoder ncd --seed 0 --test_size 0.8
-python test.py --dataset EDMCUP --strategy random --cdm facd --device cuda:0 --lr 5e-3 --batch_size 32 --num_epochs 2 --decoder ncd --seed 0 --test_size 0.8 --test_length 15 
-
-python train.py --dataset NeurIPS2020 --cdm facd --device cuda:0 --lr 3e-3 --batch_size 128 --num_epochs 1 --decoder irt --seed 0 --test_size 0.8
-python test.py --dataset NeurIPS2020 --strategy random --cdm facd --device cuda:0 --lr 3e-3 --batch_size 128 --num_epochs 2 --decoder irt --seed 0 --test_size 0.8 --test_length 15 
-
-python train.py --dataset NeurIPS2020 --cdm facd --device cuda:0 --lr 3e-3 --batch_size 128 --num_epochs 1 --decoder ncd --seed 0 --test_size 0.8
-python test.py --dataset NeurIPS2020 --strategy random --cdm facd --device cuda:0 --lr 3e-3 --batch_size 128 --num_epochs 2 --decoder ncd --seed 0 --test_size 0.8 --test_length 15 
+python test.py --dataset FrcSub --strategy BECAT --cdm facd --device cuda:0 --lr 3e-3 --batch_size 128 --num_epochs 2 --decoder ncd --seed 0 --test_size 0.8 --test_length 15
